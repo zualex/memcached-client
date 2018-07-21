2c066a75b44415519229ec613b9af917c15b70d0
zualex <Azubarev32@yandex.ru>
2018-07-21 15:26:35 +0300

    add server

--- src/Client.php
+++ src/Client.php
@@ -4,8 +4,82 @@ namespace zualex\Memcached;
 
 class Client
 {
-    public static function hello()
+    /**
+     * $server list of the servers in the pool
+     *
+     * Example:
+     *     [
+     *         [
+     *             'host' => host,
+     *             'port' => port,
+     *             'weight' => weight
+     *         ],
+     *     ]
+     * 
+     * @var array
+     */
+    protected $serverList = array();
+
+    /**
+     * Get the list of the servers in the pool
+     * 
+     * @return array
+     */
+    public function getServerList()
     {
-        return 'Hello I\'am new class';
+        return array_values($this->serverList);
+    }
+
+    /**
+     * Add a serer to the server pool
+     *
+     * @param   string  $host   hostname of the memcache server
+     * @param   int     $port   port on which memcache is running, default 11211
+     * @param   int     $weight weight of the server relative to the total weight of all the servers in the pool
+     * @return  boolean
+     */
+    public function addServer($host, $port = 11211, $weight = 0)
+    {
+        if ($this->isServerDuplicate($host, $port, $weight)) {
+            return false;
+        }
+
+        $key = $this->getServerKey($host, $port, $weight);
+        $this->serverList[$key] = [
+            'host'  => $host,
+            'port'  => $port,
+            'weight'    => $weight,
+        ];
+
+        return true;
+    }
+
+    /**
+     * Check server duplicate
+     * 
+     * @param  string  $host   hostname of the memcache server
+     * @param  int     $port   port on which memcache is running
+     * @param  int     $weight weight of the server relative to the total weight of all the servers in the pool
+     * 
+     * @return boolean
+     */
+    protected function isServerDuplicate($host, $port, $weight)
+    {
+        $key = $this->getServerKey($host, $port, $weight);
+
+        return isset($this->serverList[$key]);
+    }
+
+    /**
+     * Get key of server array
+     *
+     * @param   string  $host
+     * @param   int     $port
+     * @param   int     $weight
+     * @return  string
+     */
+    protected function getServerKey($host, $port = 11211, $weight = 0)
+    {
+        return "{$host}_{$port}_{$weight}";
     }
 }
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -7,8 +7,57 @@ use PHPUnit\Framework\TestCase;
 
 class ClientTest extends TestCase
 {
-    public function testExample()
+    const DEFAULT_PORT = 11211;
+    const DEFAULT_WEIGHT = 0;
+
+    public function testAddServer()
     {
-        $this->assertEquals('Hello I\'am new class', Client::hello());
+        $m = new Client;
+        $resultAdd = $m->addServer('test.host', 80, 100);
+        $expected = [[
+            'host' => 'test.host',
+            'port' => 80,
+            'weight' => 100,
+        ]];
+
+        $this->assertEquals(true, $resultAdd);
+        $this->assertEquals($expected, $m->getServerList());
+    }
+
+    public function testAddServerDefaultArg()
+    {
+        $m = new Client;
+        $m->addServer('test.host');
+        $expected = [[
+            'host' => 'test.host',
+            'port' => self::DEFAULT_PORT,
+            'weight' => self::DEFAULT_WEIGHT,
+        ]];
+
+        $this->assertEquals($expected, $m->getServerList());
+    }
+
+    public function testAddTwoServer()
+    {
+        $m = new Client;
+        $m->addServer('test.host');
+        $m->addServer('test2.host');
+        $serverList = $m->getServerList();
+
+        $this->assertEquals(2, count($serverList));
+        $this->assertEquals('test.host', $serverList[0]['host']);
+        $this->assertEquals('test2.host', $serverList[1]['host']);
+    }
+
+    public function testAddDuplicateServer()
+    {
+        $m = new Client;
+        $resultAddOk = $m->addServer('test.host');
+        $resultAddFail = $m->addServer('test.host');
+        $serverList = $m->getServerList();
+
+        $this->assertEquals(true, $resultAddOk);
+        $this->assertEquals(false, $resultAddFail);
+        $this->assertEquals(1, count($serverList));
     }
 }
